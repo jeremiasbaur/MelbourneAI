9cb38b1683e1f9da6a87e286a2f23e11b993a2cd
Jeremias Baur <baurjeremias@gmail.com>
2023-03-24 14:01:06 +1100

    current state

--- search/coordinate_system.py
+++ search/coordinate_system.py
@@ -51,7 +51,10 @@ class CoordinateSystem():
     def set(self, r, q, color, power):
         if color not in ['r','b','e']:
             raise Exception("Impossible color!")
-        self._coordinate_system[r%7][q%7] = (color, power)
+        if power>6:
+            self._coordinate_system[r%7][q%7] = ('e', 0)
+        else:    
+            self._coordinate_system[r%7][q%7] = (color, power)
     
     def import_state(self, given_state):
         """
@@ -99,3 +102,107 @@ class CoordinateSystem():
             self.set(new_cell[0], new_cell[1], current_cell[0], new_cell_state[1]+1)
 
         self.set(r, q, 'e', 0)
+
+    def calculate_distance(self, r1, q1, r2, q2) -> int:
+        # uses approximation:
+        return max(abs(r1-r2),abs(q1-q2))
+
+        
+class CoordinateSystem2():
+    """
+    This is the state class of the game board.
+    The board is stored in a 2D array of lists.
+    """
+    _coordinate_system = dict()
+
+    def __init__(self) -> None:
+        self._coordinate_system = dict()
+
+    def __str__(self) -> str:
+        return render_board(self.convert_to_dict(), ansi=True)
+
+    def __hash__(self) -> int:
+        return hash(tuple(sorted(list(self._coordinate_system.items()))))
+        
+    def __lt__(self, other):
+        return self.find_non_empty_cells()['r'] < other.find_non_empty_cells()['r']
+    
+    def __le__(self, other):
+        return self.find_non_empty_cells()['r'] <= other.find_non_empty_cells()['r']
+
+    def __copy__(self):
+        pass
+
+    def convert_to_dict(self) -> dict:
+        return self._coordinate_system
+
+    def at(self, r, q) -> tuple:
+        if (r%7, q%7) not in self._coordinate_system:
+            return ('e',0)
+        return self._coordinate_system[(r%7, q%7)]
+    
+    def set(self, r, q, color, power):
+        if color not in ['r','b','e']:
+            raise Exception("Impossible color!")
+        if power>6:
+            self._coordinate_system.pop((r%7,q%7))
+        elif color=='e':
+            self._coordinate_system.pop((r%7,q%7))
+        else:
+            self._coordinate_system[(r%7,q%7)] = (color, power)
+    
+    def import_state(self, given_state):
+        """
+        loads the given state into the CoordinateSystem
+
+        given_state: dict of state in the format of the class
+        """
+        for key, value in given_state.items():
+            self._coordinate_system[(key[0], key[1])] = value # r is first coordinate and q is second coordinate
+
+    def import_state_list(self, given_state_list):
+        self._coordinate_system = deepcopy(given_state_list)
+
+    def export_state_list(self) -> list:
+        return self._coordinate_system
+
+    def find_non_empty_cells(self) -> dict:
+        cells = dict()
+        cells['r'] = []
+        cells['b'] = []
+
+        for key, item in self._coordinate_system.items():
+            cells[item[0]].append((key[0], key[1], item[1]))
+        return cells
+    
+    def apply_spread(self,r,q,d_r,d_q):
+        """
+        given a cell, apply a spread from (r,q) into the d_r and d_q direction
+        """
+        if (d_r,d_q) not in [(-1,1),(0,1),(1,0),(1,-1),(0,-1),(-1,0)]:
+            raise Exception(f'Invalid spread direction!\n{(d_r,d_q)} is not possible!')
+
+        current_cell = self.at(r,q)
+
+        if current_cell[0] == 'e':
+            raise Exception("Not possible to spread because empty cell!!")
+
+        for i in range(current_cell[1]+1): # get the power of cell and spread into the direction according to given power
+            new_cell = (r + d_r*i, q + d_q*i) 
+
+            new_cell_state = self.at(new_cell[0],new_cell[1])
+
+            self.set(new_cell[0], new_cell[1], current_cell[0], new_cell_state[1]+1)
+
+        self.set(r, q, 'e', 0)
+
+    def calculate_distance(self, r1, q1, r2, q2) -> int:
+        # uses approximation:
+        return max(abs(r1-r2),abs(q1-q2))
+    
+    def calculate_distance2(self, r1, q1, r2, q2) -> int:
+        return (abs(r1-r2)+abs(q1+r1-r2-q2)+abs(q1-q2))/2
+
+        
+
+    
--- search/program.py
+++ search/program.py
@@ -1,9 +1,11 @@
 # COMP30024 Artificial Intelligence, Semester 1 2023
 # Project Part A: Single Player Infexion
 import heapq
+from copy import deepcopy
+
 
 from .utils import render_board
-from .coordinate_system import CoordinateSystem
+from .coordinate_system import CoordinateSystem, CoordinateSystem2
 
 def search(input: dict[tuple, tuple]) -> list[tuple]:
     """
@@ -20,51 +22,90 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
     # to True to see a colour-coded version (if your terminal supports it).
     
     q = [] # heap
-    explored = set()
+    explored = dict()
     backtracking = dict()
 
-    cs = CoordinateSystem() # initial state
+    cs = CoordinateSystem2() # initial state
     cs.import_state(input)
     heapq.heappush(q, (0,cs)) # q.put((0,cs))
-    explored.add(hash(cs))
+    explored[hash(cs)] = 0
     counter = 0
 
+    for i in range(7):
+        for j in range(7):
+            if i==3 and j==3:
+                continue
+            dis = cs.calculate_distance2(3,3,i,j)
+            cs.set(i,j,'r',dis)
+    print(cs)
+    
     start_state = hash(cs)
     final_state = None
     while len(q) != 0: # q.empty() is False:
         new_item = heapq.heappop(q)
+        # new_item = q.pop()
         current_state = new_item[1] # q.get()[1]
         non_empty_cells = current_state.find_non_empty_cells()
+        counter += 1
 
         # TODO: add final state reached check
         if len(non_empty_cells['b'])==0:
             final_state = hash(current_state)
-            # print("reached final state!")
+            print("reached final state!", counter)
             break
+        
+        red_cell = ""
+        min_distance = -1
+        for red in non_empty_cells['r']:
+            for blue in non_empty_cells['b']:
+                if min_distance==-1:
+                    red_cell = (red[0],red[1])
+                    min_distance = current_state.calculate_distance(red[0],red[1], blue[0], blue[1])
+                else:
+                    red_cell = (red[0],red[1])
+                    min_distance = min(min_distance, current_state.calculate_distance(red[0],red[1], blue[0], blue[1]))
 
-        for red_cell in non_empty_cells['r']:
-            for spread_dir in [(-1,1),(0,1),(1,0),(1,-1),(0,-1),(-1,0)]:
-                new_state = CoordinateSystem()
-                new_state.import_state_list(current_state.export_state_list())
+        #for red_cell in non_empty_cells['r']:
+        for spread_dir in [(-1,1),(0,1),(1,0),(1,-1),(0,-1),(-1,0)]:
+            new_state = CoordinateSystem2()
+            new_state = deepcopy(current_state) # new_state.import_state_list(current_state.export_state_list())
 
-                new_state.apply_spread(red_cell[0],red_cell[1], spread_dir[0],spread_dir[1])
-                nec = new_state.find_non_empty_cells()
+            new_state.apply_spread(red_cell[0],red_cell[1], spread_dir[0],spread_dir[1])
+            nec = new_state.find_non_empty_cells()
+            
+            move = (red_cell[0],red_cell[1], spread_dir[0],spread_dir[1])
+
+            if hash(new_state) not in explored:
+                min_distance = -1
+                for red in nec['r']:
+                    for blue in nec['b']:
+                        if min_distance==-1: min_distance = new_state.calculate_distance(red[0],red[1], blue[0], blue[1])
+                        else:
+                            min_distance = min(min_distance, new_state.calculate_distance(red[0],red[1], blue[0], blue[1]))
+
+                #cost = 0
+                #if min_distance==1:
+                #    cost = len(nec['b'])
+                    #q.clear()
+                #else:
+                cost = new_item[0]+1 + min_distance
                 
-                move = (red_cell[0],red_cell[1], spread_dir[0],spread_dir[1])
-
-                if hash(new_state) not in explored:
-                    # item = (len(nec['b']), new_state, hash(current_state), move)
-                    item = (new_item[0]+1, new_state, hash(current_state), move)
-                    heapq.heappush(q, item) # q.put((len(nec['b']), new_state))
-                    explored.add(hash(new_state))
-                    backtracking[hash(new_state)] = item
-
+                # item = (len(nec['b']), new_state, hash(current_state), move)
+                item = (cost, new_state, hash(current_state), move)
+                heapq.heappush(q, item) # q.put((len(nec['b']), new_state))
+                # q.append(item)
+                explored[hash(new_state)] = new_item[0]+1
+                backtracking[hash(new_state)] = item
+            
     moves = []
+    moves_states = []
     current_state = final_state
     while current_state!=start_state:
         move = backtracking[current_state]
         moves.append(move[3])
+        moves_states.append(move[1])
         current_state = move[2]
+        #print(move[1])
 
     """while len(q)!=0:
         state = heapq.heappop(q)[1]
@@ -76,6 +117,10 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
     #print(render_board(input, ansi=False))
 
     moves.reverse()
+    moves_states.reverse()
+    if True:
+        for i in moves_states:
+            print("")
 
     # Here we're returning "hardcoded" actions for the given test.csv file.
     # Of course, you'll need to replace this with an actual solution...
